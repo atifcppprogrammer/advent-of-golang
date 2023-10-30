f4419069f6e148c491bea9add537b2d500c346d8
atifcppprogrammer <54069350+atifcppprogrammer@users.noreply.github.com>
2023-10-31 04:07:15 +0500

    improvement: year-2022/day-01
    - completed solution for part-2 with tests

--- solutions/year-2022/day-01/main_test.go
+++ solutions/year-2022/day-01/main_test.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"adventofgolang/packages/utilities"
 	"testing"
 )
+
+func setSolutionPartEnv(t *testing.T, solutionPart string) {
+	t.Setenv("PART", solutionPart)
+	utilities.SetSolutionPart()
+}
 
 func TestSolution(t *testing.T) {
 	t.Run("solves example input correctly", func(t *testing.T) {
@@ -23,18 +29,67 @@ func TestSolution(t *testing.T) {
 			"10000",
 		}
 
-		expected, received := 24000, solution(lines)
-		if expected != received {
-			t.Errorf("Expected maxCalorieCount %q received %q", expected, received)
+		testCases := []struct {
+			solutionPart string
+			expected     int
+		}{
+			{"1", 24000},
+			{"2", 45000},
+		}
+
+		for _, testCase := range testCases {
+			setSolutionPartEnv(t, testCase.solutionPart)
+			expected, received := testCase.expected, solution(lines)
+			if expected != received {
+				t.Errorf("Expected maxCalorieCount %q received %q", expected, received)
+			}
 		}
 	})
 }
 
-func TestMax(t *testing.T) {
-	t.Run("returns the greater value of the provided arguments", func(t *testing.T) {
-		expected, received := 2, max(1, 2)
-		if expected != received {
-			t.Errorf("Expected maxCalorieCount %q received %q", expected, received)
+func TestSetMaxCalories(t *testing.T) {
+	t.Run("returns a sorted list when a < sum", func(t *testing.T) {
+		var (
+			currentSum       = 5
+			maxCalories      = []int{4, 3, 2}
+			expectedCalories = []int{5, 4, 3}
+		)
+		setMaxCalories(maxCalories, currentSum)
+		for index, calorie := range expectedCalories {
+			expected, received := calorie, maxCalories[index]
+			if expected != received {
+				t.Errorf("Expected calorie %q at index %q, received %q", expected, index, received)
+			}
+		}
+	})
+
+	t.Run("returns a sorted list when b < sum < a", func(t *testing.T) {
+		var (
+			currentSum       = 4
+			maxCalories      = []int{4, 3, 2}
+			expectedCalories = []int{4, 4, 3}
+		)
+		setMaxCalories(maxCalories, currentSum)
+		for index, calorie := range expectedCalories {
+			expected, received := calorie, maxCalories[index]
+			if expected != received {
+				t.Errorf("Expected calorie %q at index %q, received %q", expected, index, received)
+			}
+		}
+	})
+
+	t.Run("returns a sorted list when c < sum < b", func(t *testing.T) {
+		var (
+			currentSum       = 3
+			maxCalories      = []int{4, 3, 2}
+			expectedCalories = []int{4, 3, 3}
+		)
+		setMaxCalories(maxCalories, currentSum)
+		for index, calorie := range expectedCalories {
+			expected, received := calorie, maxCalories[index]
+			if expected != received {
+				t.Errorf("Expected calorie %q at index %q, received %q", expected, index, received)
+			}
 		}
 	})
 }
