b753e77c8fa61b71d10f640bd644c8986f512903
atifcppprogrammer <54069350+atifcppprogrammer@users.noreply.github.com>
2023-10-31 01:25:35 +0500

    feat: ensured that `make test` runs relevant tests for branches (improvement|solution)/yyyy/dd

--- scripts/makefile-test.sh
+++ scripts/makefile-test.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+git_branch=$(git rev-parse --abbrev-ref HEAD)
+is_puzzle_branch=$(echo $git_branch | grep -oE '(solution|improvement)\/[0-9]{4}\/[0-9]{2}' | wc -l)
+
+if [[ $is_puzzle_branch -eq 1 ]]; then 
+  year=$(echo $git_branch | cut -d '/' -f2)
+  day0=$(echo $git_branch | cut -d '/' -f3)
+  test_paths=(solutions/year-$year/day-$day0)
+else 
+  test_paths=($(find . -name go.mod | sort | xargs dirname))
+fi
+
 root_path=$(pwd)
-test_paths=($(find . -name go.mod | sort | xargs dirname))
 for test_path in ${test_paths[@]}; do
   cd $test_path && go test -v ./... 
   if [[ $? -ne 0 ]]; then exit 1; fi
